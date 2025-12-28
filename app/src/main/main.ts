cd1f806d33e627462ba8467876f96334a5a37496
Tobias <154422539+Tobyy85@users.noreply.github.com>
2025-12-28 10:20:18 +0100

    chore(aliases): add aliases for electron main process

--- app/src/main/main.ts
+++ app/src/main/main.ts
@@ -1,6 +1,6 @@
 import { app, BrowserWindow } from 'electron'
 
-import { createWindow } from './managers/windowManager'
+import { createWindow } from '@main/managers/windowManager'
 
 const isDev = !app.isPackaged
 
