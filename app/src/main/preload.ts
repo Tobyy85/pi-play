b2cfa4eb1e578d89270835c36f368a8d63a4fb73
Tobias <154422539+Tobyy85@users.noreply.github.com>
2025-12-29 21:14:12 +0100

    feat(preload): implement Arduino communication API in Electron

--- app/src/main/preload.ts
+++ app/src/main/preload.ts
@@ -1,3 +1,24 @@
-import { contextBridge } from 'electron'
+import { contextBridge, ipcRenderer } from 'electron'
 
-contextBridge.exposeInMainWorld('api', {})
+import type { ArduinoData } from '@shared/types/arduino'
+
+const electronApi = {
+    arduino: {
+        subscribeToData: (callback: (arduinoData: ArduinoData) => void) => {
+            ipcRenderer.on('arduino:change', (_event, data: ArduinoData) => {
+                callback(data)
+            })
+        },
+        subscribeToType: (type: ArduinoData['type'], callback: (value: ArduinoData['value']) => void) => {
+            ipcRenderer.on('arduino:change', (_event, data: ArduinoData) => {
+                if (data.type === type) {
+                    callback(data.value)
+                }
+            })
+        },
+    },
+}
+
+export type ElectronApi = typeof electronApi
+
+contextBridge.exposeInMainWorld('api', electronApi)
