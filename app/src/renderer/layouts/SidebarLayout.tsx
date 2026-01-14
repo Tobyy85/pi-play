7fc1cccf6efabf30b7272dafd8a4d5c0f569d741
Tobias <154422539+Tobyy85@users.noreply.github.com>
2026-01-14 20:54:22 +0100

    feat(SidebarLayout): add SidebarLayout component to structure sidebar and main content

---
+++ app/src/renderer/layouts/SidebarLayout.tsx
@@ -0,0 +1,19 @@
+import Sidebar from '@renderer/features/Sidebar/components/Sidebar'
+
+interface SidebarLayoutProps {
+    background: React.ReactNode
+    children: React.ReactNode
+}
+
+const SidebarLayout = ({ background, children }: SidebarLayoutProps) => {
+    return (
+        <div>
+            <div className='static top-0 left-0 h-dvh w-dvw'>{background}</div>
+            <div className='gap absolute top-0 left-0 flex h-dvh w-dvw gap-10 p-2'>
+                <Sidebar />
+                <main className='w-full'>{children}</main>
+            </div>
+        </div>
+    )
+}
+export default SidebarLayout
