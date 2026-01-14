3e3d8ae48fa16dde582f3c3d94aba247f6169910
Tobias <154422539+Tobyy85@users.noreply.github.com>
2026-01-14 20:53:57 +0100

    feat(Sidebar): add Sidebar component to display SidebarInfo and AppList

---
+++ app/src/renderer/features/Sidebar/components/Sidebar.tsx
@@ -0,0 +1,14 @@
+import AppList from '@renderer/features/Sidebar/components/AppList'
+import SidebarInfo from '@renderer/features/Sidebar/components/SidebarInfo'
+
+const Sidebar = () => {
+    return (
+        <>
+            <div className='rounded-squircle flex h-full w-24 shrink-0 flex-col gap-3 bg-black/50 p-2'>
+                <SidebarInfo />
+                <AppList />
+            </div>
+        </>
+    )
+}
+export default Sidebar
