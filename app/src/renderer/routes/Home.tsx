169558ad90457e4e4d6442e4f7383d46eb93bc47
Tobias <154422539+Tobyy85@users.noreply.github.com>
2026-01-16 19:05:20 +0100

    feat(Home): integrate SidebarLayout to Home route

--- app/src/renderer/routes/Home.tsx
+++ app/src/renderer/routes/Home.tsx
@@ -1,8 +1,14 @@
+import SidebarLayout from '@renderer/layouts/SidebarLayout'
+
+const HomeBackground = () => {
+    return <div className='h-full w-full bg-blue-500' />
+}
+
 const HomeRoute = () => {
     return (
-        <>
+        <SidebarLayout background={<HomeBackground />}>
             <h1>HomePage</h1>
-        </>
+        </SidebarLayout>
     )
 }
 export default HomeRoute
