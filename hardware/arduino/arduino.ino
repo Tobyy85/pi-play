4af61a6ca4cd378c2ee476b13989b957e069cab6
Tobias <154422539+Tobyy85@users.noreply.github.com>
2026-01-16 15:35:09 +0100

    feat(arduino): implement temperature reading and change detection for thermistor

--- hardware/arduino/arduino.ino
+++ hardware/arduino/arduino.ino
@@ -1,8 +1,23 @@
 #include <Arduino.h>
 
+#include "serial_communication.h"
+#include "src/ChangeDetector.h"
+
+#include "src/Thermistor.h"
+
+Thermistor thermistor(A0, 100000.0f, 100000.0f, 25.0f, 3950.0f);
+ChangeDetector<float> thermistorChangeDetector(0.5f);
+
+
+
 void setup() {
+    SerialCommunication::begin(115200);
     Serial.begin(115200);
 }
 
 void loop() {
+    float temperatureC = thermistor.readTemperatureCelsiusAvg(10, 5);
+    if (!isnan(temperatureC) && thermistorChangeDetector.hasChanged(temperatureC)) {
+        SerialCommunication::sendJson("temperature", String(temperatureC, 2));
+    }
 }
