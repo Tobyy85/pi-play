d92064ef06c89260d5ae5bc637c7bd55ce5ca271
Tobias <154422539+Tobyy85@users.noreply.github.com>
2026-01-17 10:54:25 +0100

    feat(arduino): integrate SensorManager and SensorHandler to arduino.ino

--- hardware/arduino/arduino.ino
+++ hardware/arduino/arduino.ino
@@ -1,23 +1,29 @@
 #include <Arduino.h>
 
 #include "src/serialCommunication.h"
-#include "src/ChangeDetector.h"
+#include "src/SensorHandler.h"
+#include "src/SensorManager.h"
 
 #include "src/sensors/Thermistor.h"
 
+SensorManager sensorManager;
+
 Thermistor thermistor(A0, 100000.0f, 100000.0f, 25.0f, 3950.0f);
-ChangeDetector<float> thermistorChangeDetector(0.5f);
+float readTemperature() {
+    return thermistor.readTemperatureCelsiusAvg(10, 5);
+}
 
-
+SensorHandler tempHandler("temperature", readTemperature, 0.5f);
 
 void setup() {
     SerialCommunication::begin(115200);
     Serial.begin(115200);
+
+    sensorManager.addSensor(&tempHandler);
 }
 
 void loop() {
-    float temperatureC = thermistor.readTemperatureCelsiusAvg(10, 5);
-    if (!isnan(temperatureC) && thermistorChangeDetector.hasChanged(temperatureC)) {
-        SerialCommunication::sendJson("temperature", String(temperatureC, 2));
-    }
+    sensorManager.updateAll();
+
+    sensorManager.checkSerialRequests();
 }
