71b6fdd4130fb105c9dfed7ee0357dd0f3c49950
Tobias <154422539+Tobyy85@users.noreply.github.com>
2026-01-19 17:13:18 +0100

    feat(preload): return unsubscribe function from subscribeToSensorId and subscribeToData

--- app/src/main/preload.ts
+++ app/src/main/preload.ts
@@ -1,23 +1,31 @@
-import { contextBridge, ipcRenderer } from 'electron'
+import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron'
 
 import type { ArduinoData } from '@shared/types/arduino'
 
 const electronApi = {
     arduino: {
-        subscribeToData: (callback: (arduinoData: ArduinoData) => void) => {
-            ipcRenderer.on('arduino:change', (_event, data: ArduinoData) => {
+        subscribeToData: (callback: (arduinoData: ArduinoData) => void): (() => void) => {
+            const handler = (_event: IpcRendererEvent, data: ArduinoData) => {
                 callback(data)
-            })
+            }
+            ipcRenderer.on('arduino:change', handler)
+            return () => {
+                ipcRenderer.off('arduino:change', handler)
+            }
         },
         subscribeToSensorId: (
             sensorId: ArduinoData['sensorId'],
             callback: (value: ArduinoData['value']) => void
-        ) => {
-            ipcRenderer.on('arduino:change', (_event, data: ArduinoData) => {
+        ): (() => void) => {
+            const handler = (_event: IpcRendererEvent, data: ArduinoData) => {
                 if (data.sensorId === sensorId) {
                     callback(data.value)
                 }
-            })
+            }
+            ipcRenderer.on('arduino:change', handler)
+            return () => {
+                ipcRenderer.off('arduino:change', handler)
+            }
         },
         requestSensorValue: (sensorId: string): Promise<ArduinoData> => {
             return ipcRenderer.invoke('arduino:requestSensorValue', sensorId)
