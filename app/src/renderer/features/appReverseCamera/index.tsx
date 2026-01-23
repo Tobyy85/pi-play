0506b484ba4f653935a3e9cdd7b7e302d24e1194
Tobias <154422539+Tobyy85@users.noreply.github.com>
2026-01-23 19:41:13 +0100

    feat(config): add CAMERA_CONFIG

--- app/src/renderer/features/appReverseCamera/index.tsx
+++ app/src/renderer/features/appReverseCamera/index.tsx
@@ -1,6 +1,8 @@
 import Camera from '@renderer/features/appReverseCamera/Camera'
 import SidebarLayout from '@renderer/layouts/SidebarLayout'
 
+import { CAMERA_CONFIG } from '@shared/config/camera'
+
 const AppReverseCamera = () => {
     return (
         <SidebarLayout background={<AppReverseCameraBackground />}>
@@ -16,7 +18,7 @@ const AppReverseCameraBackground = () => {
         <div className='size-full bg-black/50'>
             <div className='size-full blur-3xl'>
                 <Camera
-                    deviceId='1fd215033257ce2031f75ef2193545b2465b8efff9e3f7d50bd69f8cbe5205a2'
+                    deviceId={CAMERA_CONFIG.reverseCameraId}
                     isMirrored={true}
                 />
             </div>
@@ -28,7 +30,7 @@ const AppReverseCameraContent = () => {
     return (
         <div className='size-full overflow-hidden rounded-md'>
             <Camera
-                deviceId='1fd215033257ce2031f75ef2193545b2465b8efff9e3f7d50bd69f8cbe5205a2'
+                deviceId={CAMERA_CONFIG.reverseCameraId}
                 isMirrored={true}
             />
         </div>
---
+++ app/src/shared/config/camera.ts
@@ -0,0 +1,7 @@
+interface CameraConfig {
+    reverseCameraId: string
+}
+
+export const CAMERA_CONFIG: CameraConfig = {
+    reverseCameraId: '1fd215033257ce2031f75ef2193545b2465b8efff9e3f7d50bd69f8cbe5205a2',
+}
