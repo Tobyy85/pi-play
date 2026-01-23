11f52317e509abe64159277b5339f5dd6cfa0664
Tobias <154422539+Tobyy85@users.noreply.github.com>
2026-01-23 12:07:28 +0100

    feat(routes): integrate ReverseTriggerLayout and  AppReverseCamera

--- app/src/renderer/routes/routes.tsx
+++ app/src/renderer/routes/routes.tsx
@@ -1,10 +1,27 @@
+import AppReverseCamera from '@renderer/features/appReverseCamera'
 import HomeRoute from '@renderer/routes/Home'
 import NotFoundRoute from '@renderer/routes/NotFound'
 
+import ReverseTriggerLayout from '@renderer/layouts/ReverseTriggerLayout'
+
 export const routes = [
     {
-        path: '/',
-        element: <HomeRoute />,
-        errorElement: <NotFoundRoute />,
+        element: <ReverseTriggerLayout />,
+        children: [
+            {
+                path: '/',
+                element: <HomeRoute />,
+                errorElement: <NotFoundRoute />,
+            },
+            {
+                path: 'apps',
+                children: [
+                    {
+                        path: 'reverse-camera',
+                        element: <AppReverseCamera />,
+                    },
+                ],
+            },
+        ],
     },
 ]
