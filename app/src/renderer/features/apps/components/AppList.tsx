d6ba6ead9e56faed82e2a681bd5d5ce1edcdeeec
Tobias <154422539+Tobyy85@users.noreply.github.com>
2026-01-23 20:37:35 +0100

    refactor(AppList): replace appList with APP_REGISTRY

--- app/src/renderer/features/apps/components/AppList.tsx
+++ app/src/renderer/features/apps/components/AppList.tsx
@@ -1,17 +1,17 @@
 import AppIcon from '@renderer/features/apps/components/AppIcon'
 
-import { appList } from '@renderer/features/apps/constants/appList'
+import APP_REGISTRY from '@renderer/features/apps/config/appRegistry'
 
 const AppList = () => {
     return (
         <div className='no-scrollbar rounded-squircle h-full overflow-y-scroll'>
             <div className='flex min-h-full flex-col justify-center gap-4'>
-                {appList.map(app => (
+                {APP_REGISTRY.map(app => (
                     <AppIcon
-                        iconSource={app.iconSource}
+                        iconSource={app.icon}
                         name={app.name}
-                        path={app.path}
-                        key={app.path}
+                        path={app.id}
+                        key={app.id}
                     />
                 ))}
             </div>
