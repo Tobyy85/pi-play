598b5635d69e85b24e4c491037996a93d54a1efc
Tobias <154422539+Tobyy85@users.noreply.github.com>
2026-01-24 10:45:55 +0100

    fix(AppList): update path to include BASE_PATH

--- app/src/renderer/features/apps/components/AppList.tsx
+++ app/src/renderer/features/apps/components/AppList.tsx
@@ -2,6 +2,8 @@ import AppIcon from '@renderer/features/apps/components/AppIcon'
 
 import APP_REGISTRY from '@renderer/features/apps/config/appRegistry'
 
+import { BASE_PATH } from '@renderer/features/apps/routes'
+
 const AppList = () => {
     return (
         <div className='no-scrollbar rounded-squircle h-full overflow-y-scroll'>
@@ -10,7 +12,7 @@ const AppList = () => {
                     <AppIcon
                         iconSource={app.icon}
                         name={app.name}
-                        path={app.id}
+                        path={`/${BASE_PATH}/${app.id}`}
                         key={app.id}
                     />
                 ))}
