ecd1d2f9daf33cc207c5cbf453cc558f9b78308b
Tobias <154422539+Tobyy85@users.noreply.github.com>
2026-01-24 10:46:04 +0100

    refactor(appRoutes): specify RouteObject type for appsRoutes and routes

--- app/src/renderer/features/apps/routes/index.tsx
+++ app/src/renderer/features/apps/routes/index.tsx
@@ -1,10 +1,11 @@
+import type { RouteObject } from 'react-router-dom'
+
 import SidebarLayout from '@renderer/layouts/SidebarLayout'
 
 import APP_REGISTRY from '@renderer/features/apps/config/appRegistry'
 
 export const BASE_PATH = 'apps'
-
-export const appsRoutes = {
+export const appsRoutes: RouteObject = {
     path: BASE_PATH,
     children: APP_REGISTRY.map(app => ({
         path: app.id,
--- app/src/renderer/routes/routes.tsx
+++ app/src/renderer/routes/routes.tsx
@@ -1,11 +1,13 @@
+import type { RouteObject } from 'react-router-dom'
+
+import ReverseTriggerLayout from '@renderer/layouts/ReverseTriggerLayout'
+
 import HomeRoute from '@renderer/routes/Home'
 import NotFoundRoute from '@renderer/routes/NotFound'
 
 import { appsRoutes } from '@renderer/features/apps/routes'
 
-import ReverseTriggerLayout from '@renderer/layouts/ReverseTriggerLayout'
-
-export const routes = [
+export const routes: RouteObject[] = [
     {
         element: <ReverseTriggerLayout />,
         children: [
