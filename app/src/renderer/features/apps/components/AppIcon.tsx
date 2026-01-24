ec241598ea074db3e2bad494b910ab3624f09c0d
Tobias <154422539+Tobyy85@users.noreply.github.com>
2026-01-24 10:49:22 +0100

    style(AppIcon): adjust NavLink and img styling

--- app/src/renderer/features/apps/components/AppIcon.tsx
+++ app/src/renderer/features/apps/components/AppIcon.tsx
@@ -12,14 +12,14 @@ const AppIcon = ({ iconSource: icon, name, path }: AppIconProps) => {
             <NavLink
                 to={path}
                 className={({ isActive }) =>
-                    `rounded-squircle flex h-20 w-20 shrink-0 flex-col items-center justify-center bg-white
-                    ${isActive ? 'brightness-100' : 'brightness-90'}`
+                    `rounded-squircle flex h-20 w-20 shrink-0 flex-col items-center justify-center
+                    overflow-hidden bg-white ${isActive ? 'brightness-100' : 'brightness-90'}`
                 }
             >
                 <img
                     src={icon}
                     alt={`${name} icon`}
-                    className='h-3/4 w-3/4 object-contain'
+                    className='size-full object-cover'
                 />
             </NavLink>
         </>
