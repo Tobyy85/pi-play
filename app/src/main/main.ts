bf64f30cd5dc08f6aff49d7a7a5ca331de3f9456
Tobias <154422539+Tobyy85@users.noreply.github.com>
2026-02-27 15:27:38 +0100

    feat(CallService): integrate CallService into main.ts

--- app/src/main/main.ts
+++ app/src/main/main.ts
@@ -1,9 +1,12 @@
 import { app, BrowserWindow } from 'electron'
 
 import WindowManager from '@main/managers/windowManager'
+
 import ArduinoService from '@main/services/arduinoService'
+import CallService from '@main/services/CallService'
 import GPSService from '@main/services/gpsService'
 import MediaPlayerService from '@main/services/MediaPlayerService'
+
 import { ARDUINO_CONFIG } from '@shared/config/arduino'
 
 const windowManager = new WindowManager()
@@ -12,6 +15,7 @@ const getWindow = () => windowManager.getWindow()
 const arduinoService = new ArduinoService(getWindow)
 const gpsService = new GPSService(getWindow)
 const mediaPlayerService = new MediaPlayerService(getWindow)
+const callService = new CallService(getWindow)
 
 app.whenReady().then(() => {
     windowManager.createWindow()
@@ -25,6 +29,9 @@ app.whenReady().then(() => {
     mediaPlayerService.registerIpcHandlers()
     mediaPlayerService.initialize()
 
+    callService.registerIpcHandlers()
+    callService.initialize()
+
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
             windowManager.createWindow()
@@ -36,6 +43,7 @@ app.on('before-quit', () => {
     arduinoService.disconnect()
     gpsService.disconnect()
     mediaPlayerService.disconnect()
+    callService.disconnect()
 })
 
 app.on('window-all-closed', () => {
