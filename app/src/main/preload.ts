cc1f03d032664e4454c3b41115bd58cf11d1a253
Tobias <154422539+Tobyy85@users.noreply.github.com>
2026-03-09 20:31:35 +0100

    feat(phoneBook): add getCallHistory method to retrieve call history

--- app/src/main/preload.ts
+++ app/src/main/preload.ts
@@ -4,7 +4,7 @@ import type { ArduinoData } from '@shared/types/arduino'
 import type { CallInfo } from '@shared/types/call'
 import type { GPSData } from '@shared/types/gps'
 import type { Position, Status, TrackInfo } from '@shared/types/mediaPlayer'
-import type { Contact } from '@shared/types/phoneBook'
+import type { CallHistoryEntry, Contact } from '@shared/types/phoneBook'
 
 const subscribeToChannel = <T>(channel: string, callback: (data: T) => void) => {
     const cb = (_event: IpcRendererEvent, data: T) => {
@@ -102,6 +102,9 @@ const electronApi = {
     phoneBook: {
         getContacts: async (): Promise<Contact[]> => {
             return await ipcRenderer.invoke('phoneBook:getContacts')
+        },
+        getCallHistory: async (): Promise<CallHistoryEntry[]> => {
+            return await ipcRenderer.invoke('phoneBook:getCallHistory')
         },
     },
 }
