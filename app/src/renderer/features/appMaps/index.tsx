14781dcdedcc10bda57006678fb872e53527a55e
Tobias <154422539+Tobyy85@users.noreply.github.com>
2026-03-12 20:09:37 +0100

    style(AppMapsContent): increase font size of speed display

--- app/src/renderer/features/appMaps/index.tsx
+++ app/src/renderer/features/appMaps/index.tsx
@@ -30,8 +30,11 @@ export const AppMapsContent = () => {
             {error && <p className='text-red-500'>Error: {error}</p>}
             {data && (
                 <div className='absolute bottom-1 left-0'>
-                    <div className='rounded-full bg-black/75 px-4 py-1 text-3xl font-bold text-white'>
-                        <span>{data.speed ?? 'N/A'}</span> <span className='text-2xl'>km/h</span>
+                    <div
+                        className='rounded-full bg-black/75 px-4 py-1.5 text-[2.5rem] leading-[1.05] font-bold
+                            text-white'
+                    >
+                        <span>{data.speed ?? 'N/A'}</span> <span className='text-4xl'>km/h</span>
                     </div>
                 </div>
             )}
