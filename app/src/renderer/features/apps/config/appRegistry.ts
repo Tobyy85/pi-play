a38eb4f0e5d3205ae8cc3041f08d2bfc3745b403
Tobias <154422539+Tobyy85@users.noreply.github.com>
2026-04-26 10:59:22 +0200

    feat(apps): change app icons

--- app/src/renderer/features/apps/config/appRegistry.ts
+++ app/src/renderer/features/apps/config/appRegistry.ts
@@ -1,13 +1,13 @@
-import ReverseCameraIcon from '@renderer/assets/reverse-camera-icon.png'
+import ReverseCameraIcon from '@renderer/assets/reverse-camera-icon.svg'
 import { AppReverseCameraBackground, AppReverseCameraContent } from '@renderer/features/appReverseCamera'
 
-import MapsIcon from '@renderer/assets/maps-icon.png'
+import MapsIcon from '@renderer/assets/maps-icon.svg'
 import { AppMapsBackground, AppMapsContent } from '@renderer/features/appMaps'
 
-import MusicIcon from '@renderer/assets/music-icon.png'
+import MusicIcon from '@renderer/assets/music-icon.svg'
 import { AppMusicBackground, AppMusicContent } from '@renderer/features/appMusic'
 
-import PhoneIcon from '@renderer/assets/phone-icon.png'
+import PhoneIcon from '@renderer/assets/phone-icon.svg'
 import { AppPhoneBackground, AppPhoneContent } from '@renderer/features/appPhone'
 
 interface AppRegistryItem {
@@ -20,11 +20,11 @@ interface AppRegistryItem {
 
 const APP_REGISTRY: AppRegistryItem[] = [
     {
-        name: 'Reverse Camera',
-        id: 'reverse-camera',
-        icon: ReverseCameraIcon,
-        contentComponent: AppReverseCameraContent,
-        backgroundComponent: AppReverseCameraBackground,
+        name: 'Music',
+        id: 'music',
+        icon: MusicIcon,
+        contentComponent: AppMusicContent,
+        backgroundComponent: AppMusicBackground,
     },
     {
         name: 'Maps',
@@ -34,18 +34,18 @@ const APP_REGISTRY: AppRegistryItem[] = [
         backgroundComponent: AppMapsBackground,
     },
     {
-        name: 'Music',
-        id: 'music',
-        icon: MusicIcon,
-        contentComponent: AppMusicContent,
-        backgroundComponent: AppMusicBackground,
-    },
-    {
         name: 'Phone',
         id: 'phone',
         icon: PhoneIcon,
         contentComponent: AppPhoneContent,
         backgroundComponent: AppPhoneBackground,
+    },
+    {
+        name: 'Reverse Camera',
+        id: 'reverse-camera',
+        icon: ReverseCameraIcon,
+        contentComponent: AppReverseCameraContent,
+        backgroundComponent: AppReverseCameraBackground,
     },
 ]
 
