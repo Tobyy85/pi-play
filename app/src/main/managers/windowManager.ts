9bc64102e23d72ccda255af3384fb7e2e4a8ac75
Tobias <154422539+Tobyy85@users.noreply.github.com>
2026-04-26 16:16:29 +0200

    feat(WindowManager): enable fullscreen mode in build and disable sandbox for native module support

--- app/src/main/managers/windowManager.ts
+++ app/src/main/managers/windowManager.ts
@@ -10,10 +10,12 @@ class WindowManager {
         this.browserWindow = new BrowserWindow({
             width: 1024,
             height: 600,
+            fullscreen: !isDev,
             icon: path.join(__dirname, '..', '..', '..', 'assets', 'icon.png'),
             autoHideMenuBar: true,
             webPreferences: {
                 preload: path.join(__dirname, '..', 'preload.js'),
+                sandbox: false, // Disable sandbox to allow native module like serialport
             },
         })
 
