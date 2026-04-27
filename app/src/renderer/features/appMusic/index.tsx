681eb6205a4c862da6113b5773f83c6008bcbf36
Tobias <154422539+Tobyy85@users.noreply.github.com>
2026-04-27 21:49:07 +0200

    feat(appMusic): Add optimistic UI for playback status

--- app/src/renderer/features/appMusic/index.tsx
+++ app/src/renderer/features/appMusic/index.tsx
@@ -1,14 +1,17 @@
+import { useEffect, useState } from 'react'
+
 import {
     usePlaybackStatus,
     usePosition,
     useTrackInfo,
 } from '@renderer/features/appMusic/hooks/useMediaPlayerData'
 
+import GradientBackground from '@renderer/features/appMusic/components/GradientBackground'
 import NowPlaying from '@renderer/features/appMusic/components/NowPlaying'
 import PlaybackControls from '@renderer/features/appMusic/components/PlaybackControls'
 import PlaybackProgress from '@renderer/features/appMusic/components/PlaybackProgress'
 
-import GradientBackground from '@renderer/features/appMusic/components/GradientBackground'
+import type { Status } from '@shared/types/mediaPlayer'
 
 export const AppMusicBackground = () => {
     const { data: trackInfo, isLoading } = useTrackInfo()
@@ -25,21 +28,37 @@ export const AppMusicContent = () => {
     const { data: status } = usePlaybackStatus()
     const { data: position } = usePosition()
 
+    const [optimisticStatus, setOptimisticStatus] = useState<Status | null>(null)
+
+    const handlePlay = async () => {
+        setOptimisticStatus('playing')
+        await window.api.mediaPlayer.actions.play()
+    }
+
+    const handlePause = async () => {
+        setOptimisticStatus('paused')
+        await window.api.mediaPlayer.actions.pause()
+    }
+
+    useEffect(() => {
+        setOptimisticStatus(status)
+    }, [status])
+
     return (
         <div className='flex size-full flex-col items-center justify-center py-2 pr-8'>
             <NowPlaying trackInfo={trackInfo} />
             <div className='flex w-full flex-col items-center justify-end gap-8'>
                 <PlaybackControls
-                    status={status}
-                    onPlay={window.api.mediaPlayer.actions.play}
-                    onPause={window.api.mediaPlayer.actions.pause}
+                    status={optimisticStatus}
+                    onPlay={handlePlay}
+                    onPause={handlePause}
                     onNext={window.api.mediaPlayer.actions.next}
                     onPrevious={window.api.mediaPlayer.actions.previous}
                 />
                 <PlaybackProgress
                     position={position}
                     duration={trackInfo ? trackInfo.duration : null}
-                    status={status}
+                    status={optimisticStatus}
                 />
             </div>
         </div>
