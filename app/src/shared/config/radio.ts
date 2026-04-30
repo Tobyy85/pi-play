1978570fb18b597d26e995828972b85e0d1a6249
Tobias <154422539+Tobyy85@users.noreply.github.com>
2026-04-30 15:42:18 +0200

    feat(radio): add initial radio configuration with API URL

---
+++ app/src/shared/config/radio.ts
@@ -0,0 +1,3 @@
+const languageCode = 'cs'
+const stationCount = 100
+export const RADIO_API_URL = `https://50k-radio-stations.p.rapidapi.com/radios?page=1&sort=popularity&limit=${stationCount}&language_code=${languageCode}`
